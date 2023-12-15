1191c3c8f6a08b0af1b17b54760516f08c235892
DanMarques31 <daniellmarques2003@hotmail.com>
2023-12-14 22:08:06 -0300

    last

--- src/func.h
+++ src/func.h
@@ -1,14 +1,15 @@
 #include <stdio.h> // Entrada e saida padrão do C
 #include <stdlib.h> // Alocação e controle de processos
 #include <string.h> // Manipulação de string
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-#include <math.h>
+#include <sys/socket.h> // Programação em socket
+#include <netinet/in.h> // Estruturas do protocolo IP
+#include <unistd.h> // Funções do sistema
+#include <arpa/inet.h> // Manipulação de endereço IP em string
+#include <sys/time.h> // Funções relacionadas a tempo
+#include <math.h> // Funções matemáticas
 
-#define TAM_BUFFER 65536
+#define TAM_BUFFER 65536 // Valor máximo de buffer
 
+// Assinaturas das funções
 void envia_msg(const char *msg);
 void calcular_e_imprimir_tempos(struct timeval *inicio, struct timeval *final, int total_bytes, int tam_buffer);
